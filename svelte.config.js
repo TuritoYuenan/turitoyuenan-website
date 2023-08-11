d00e5e4042b0a90835215afd038dfc3daf73bee0
TuritoYuenan <turitoyuenan@proton.me>
2023-08-11 22:27:14 +0700

    change to cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
+// Check whether the website is in dev mode
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex({ extension: '.md' })]
+	preprocess: [vitePreprocess(), mdsvex({ extension: '.md' })],
+	kit: { adapter: adapter() }
 };
 
 export default config;
