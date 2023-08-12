d2949a407205700ad7221345d6697971a46d39ae
TuritoYuenan <turitoyuenan@proton.me>
2023-08-12 21:58:21 +0700

    add my own curriculum vitae

--- src/lib/components/Parallax.svelte
+++ src/lib/components/Parallax.svelte
@@ -5,17 +5,20 @@
 <style lang="scss">
 	@use '$lib/styles/macchiato' as *;
 	$tint: transparentize($base, 0.5);
+	$wallpaper: url(/wallpaper.jpg);
 
 	section#parallax {
-		min-height: 70vh;
+		min-height: 60vh;
+		padding: 1rem;
 		color: $yellow;
+		text-align: center;
 
 		display: flex;
 		justify-content: center;
 		align-items: center;
 
 		background: {
-			image: linear-gradient(0deg, $tint, $tint), url(/sample-wallpaper.jpg);
+			image: linear-gradient(0deg, $tint, $tint), $wallpaper;
 			attachment: fixed;
 			position: center;
 			repeat: no-repeat;
@@ -23,7 +26,7 @@
 		}
 
 		h1 {
-			font-size: 8vw;
+			font-size: 10vh;
 			font-variant: small-caps;
 			filter: drop-shadow(0 0 10px $yellow);
 		}
---
+++ src/routes/crrvitae/+page.svelte
@@ -0,0 +1,17 @@
+<article>
+	<h1>My CV</h1>
+	<iframe src="/CV.pdf" title="Triet's Curriculum Vitae" />
+</article>
+
+<style lang="scss">
+	h1 {
+		text-align: center;
+	}
+
+	iframe {
+		width: 100%;
+		height: 70vh;
+		margin: 1em 0;
+		border: none;
+	}
+</style>
