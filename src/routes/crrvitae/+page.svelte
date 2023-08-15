92a12452fa418d80a1e35e29c437380aef742699
TuritoYuenan <turitoyuenan@proton.me>
2023-08-15 22:24:27 +0700

    get cv from google drive instead

--- src/routes/crrvitae/+page.svelte
+++ src/routes/crrvitae/+page.svelte
@@ -1,17 +1,31 @@
+<script>
+	const title = `Triet's Curriculum Vitae`;
+	const src = `https://drive.google.com/file/d/1ZdrqAAYJ8mUP1uIBY4aJyyjxVVpSXprH/preview`;
+</script>
+
 <article>
 	<h1>My CV</h1>
-	<iframe src="/CV.pdf" title="Triet's Curriculum Vitae" />
+	<section><iframe {src} {title} /></section>
 </article>
 
 <style lang="scss">
+	@use '$styles/macchiato' as *;
+
 	h1 {
 		text-align: center;
 	}
 
+	section {
+		height: 70vh;
+		margin: 1rem 0;
+		padding: 1rem;
+		border-radius: 1rem;
+		background: $mantle;
+	}
+
 	iframe {
 		width: 100%;
-		height: 70vh;
-		margin: 1em 0;
+		height: 100%;
 		border: none;
 	}
 </style>
