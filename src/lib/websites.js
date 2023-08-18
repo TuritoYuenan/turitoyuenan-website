d909d7c1e989fe7f179e1fea7f8068f6bc7a4494
TuritoYuenan <turitoyuenan@proton.me>
2023-08-18 23:01:12 +0700

    organise cv page

---
+++ src/lib/websites.js
@@ -0,0 +1,7 @@
+// Return URL links for popular websites
+
+/**
+ * Returns an Artstation link
+ * @param path
+ */
+export const artStation = (path) => `https://artstation.com/${path}`;
--- src/routes/crrvitae/+page.svelte
+++ src/routes/crrvitae/+page.svelte
@@ -2,11 +2,17 @@
 	import { variants } from '@catppuccin/palette';
 	import Header from '../../lib/components/Header.svelte';
 	import Metadata from '../../lib/components/Metadata.svelte';
+
+	const title = 'My CV';
+	const src = 'https://drive.google.com/file/d/1ZdrqAAYJ8mUP1uIBY4aJyyjxVVpSXprH/preview';
 </script>
 
 <Metadata {title} />
 
 <Header {title} color={variants.macchiato.blue.hex} />
+<section>
+	<iframe {src} title="Triet's Curriculum Vitae" />
+</section>
 
 <style>
 	section {
